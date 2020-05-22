b9764a6726b9aacff296f4fb39bc1fe5ab8042dc
Augusto Nunes <nunes.augusto19@gmail.com>
2020-05-22 18:37:24 -0300

    Atualizando arquivos

--- data-struc/palind.py
+++
@@ -1,9 +0,0 @@
-def is_palind(s:str):
-    s = s.lower()
-    for i in range(len(s)):
-        if not s[i] == s[-(i+1)]:
-            return False
-    return True
-
-while True:
-    print(is_palind(input()))
--- julia-lang/wrapper/teste.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    return 0;
-}
-
-int is_even(int x){
-    return x%2 == 0;
-}
