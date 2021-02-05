4b855c657f7eb6f1d63ec6d4c02d04cc7383622c
Augusto Nunes <nunes.augusto19@gmail.com>
2021-02-05 00:27:53 +0000

    corrigindo erro

--- src_py/gradient/teste_conjuntos.py
+++ src_py/gradient/teste_conjuntos.py
@@ -15,17 +15,13 @@ t = np.array([0.075, 0.812, 0.317, 0.581, 0.752, 0.994, 0.967, 0.511, 0.851,
               0.925, 0.842, 0.295, 0.633, 0.522, 0.306])
 bot = CjsBot()
 seeds = [661, 308, 769, 343, 491]
-np.random.seed(45)
-pontos = np.random.random((500,i))*1 +0.001
-
 for seed in seeds:
     np.random.seed(seed)
     bot.send_message(f"Estou na seed {seed}")
-    pontos = np.random.random((500,dim))*1 +0.001
     if not os.path.isdir(f'./teste_conjuntos/{seed}/'):
         os.mkdir(f'./teste_conjuntos/{seed}/')
     for dim in range(1,16):
-        bot.send_message(f"Estou na dimensão {i}")
+        bot.send_message(f"Estou na dimensão {dim}")
         links = ["../../networks/c-town/nodes", 
             "../../networks/c-town/links", 
             "../../networks/c-town/rede.inp", 
@@ -40,6 +36,7 @@ for seed in seeds:
         x = []
         y = []
         dists = []
+        pontos = np.random.random((500,dim))*1 +0.001
         for i in range(pontos.shape[0]):
             try:
                 o = optimize.minimize(net.objetivo, pontos[i,:], method='Nelder-Mead')
@@ -51,7 +48,7 @@ for seed in seeds:
                 continue
         dists = np.array(dists)
         sts = f'mode:{stats.mode(dists)}\nsd:{np.array(dists).std()}'
-        open(f'./teste_conjuntos/{dim}/stats.txt', 'a').write(sts).close()
+        open(f'./teste_conjuntos/{seed}/{dim}/stats.txt', 'a').write(sts)
     
         arq = open(f'./teste_conjuntos/{seed}/{dim}/y.txt','a')
         np.savetxt(arq, y)
@@ -61,7 +58,7 @@ for seed in seeds:
         np.savetxt(arq, x)
         arq.close()
 
-        arq = open(f'./teste_conjuntos/{seed}/{dim}/dists.txt')
+        arq = open(f'./teste_conjuntos/{seed}/{dim}/dists.txt', 'a')
         np.savetxt(arq, dists)
         arq.close()
     
