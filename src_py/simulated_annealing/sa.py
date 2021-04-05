2bdf41a6a88677f1ef94bd324dd34bbeeaea8bc9
Augusto Nunes <nunes.augusto19@gamil.com>
2021-04-04 23:30:52 -0300

    relatorio parcial

--- src_py/simulated_annealing/sa.py
+++ src_py/simulated_annealing/sa.py
@@ -7,7 +7,7 @@ def f(x):
     return x[0]**2 + x[1]**2 
 
 
-def simulated_annealing(x0, f, t0, h, iter_MAX):
+def simulated_annealing(x0, f, t0, h=0.01, iter_MAX=200):
     x = x0
     x_bsf = x0
     t = t0 #aquece até a temperatura t0
@@ -15,7 +15,7 @@ def simulated_annealing(x0, f, t0, h, iter_MAX):
     while t > h: # até que atinja a temperatura mínima para parar
         iter = 0
         while iter < iter_MAX: # até que atinja o equilíbrio na temperatura atual
-            y = x + np.random.uniform(-1,1,len(x)) # perturbe o x 
+            y = x + np.random.uniform(-1,1,len(x))*h # perturbe o x 
             delta_xy = f(y) - f(x) # faça a variação do custo, se negativa, então y tem um custo menor
             if (delta_xy <= 0) or np.random.normal() < np.exp(-delta_xy/t): # a aceitação do novo ponto segue o critério de metropolis
                 x = y
@@ -23,7 +23,7 @@ def simulated_annealing(x0, f, t0, h, iter_MAX):
                     x_bsf = x 
             iter +=1
             print(f"t={t}   iter={iter}   x={x}")
-        t -= 0.1*t
+        t -= h*t
     return x_bsf
 
 a = simulated_annealing([100,200],f, 100, 0.0000001, 50 )
